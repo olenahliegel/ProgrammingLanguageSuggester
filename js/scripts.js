431f6222f2258ebd101eaca60b1b01c539cb6f04
Olena Shliegel <olenashliegel@gmail.com>
2023-11-04 18:08:41 -0700

    It will add full stack form and functionaliuty

--- js/scripts.js
+++ js/scripts.js
@@ -74,6 +74,54 @@ function recommendationBackEndLanguage(currentExp, projectTypes, microServices, 
   return "Java";
 }
 
+function recommendationBackEndLanguage(fullstackExperience, fullstackInterests, multitasker, fullstackEvents) {
+  let result = 0;
+  if (fullstackExperience === "fullstackBeginner")
+    result += 1;
+  else if (fullstackExperience === "fullstackDabbler")
+    result += 2;
+  else if (fullstackExperience === "fullstackExp")
+    result += 3;
+  else if (fullstackExperience === "fullstackPro")
+    result += 4;
+
+  if (fullstackInterests === "interfaces")
+    result += 1;
+  else if (fullstackInterests === "serverSide")
+    result += 2;
+  else if (fullstackInterests === "endToEnd")
+    result += 3;
+  else if (fullstackInterests === "multipleLanguages")
+    result += 4;
+
+  if (multitasker === "codeJuggler")
+    result += 1;
+  else if (multitasker === "onlyJoint")
+    result += 2;
+  else if (multitasker === "confused")
+    result += 3;
+  else if (multitasker === "proJuggler")
+    result += 4;
+
+  if (fullstackEvents === "codeRelay")
+    result += 1;
+  else if (fullstackEvents === "debuggingMarathon")
+    result += 2;
+  else if (fullstackEvents === "designDiving")
+    result += 3;
+  else if (fullstackEvents === "languageTriathlon")
+    result += 4;
+
+  if (result <= 4)
+    return "Java Script";
+  else if (result >= 5 && result <= 8)
+    return "Python";
+  else if (result > 8 && result <= 12)
+    return "Ruby";
+  else
+    return "Java";
+}
+
 function outputForFirstAnswerResult(answer) {
   const frontEndOutput = "Your current experience level with coding and web development is: "
   if (answer === "frontEndBeginner") {
@@ -220,6 +268,62 @@ function resetBackForm() {
   document.querySelector("div#error").setAttribute("class", "hidden");
 }
 
+//FullStack questionary form
+function handleFullstackAnswers(event) {
+  event.preventDefault();
+
+  const fullstackExperience = document.querySelector("input[name='fullstackExperience']:checked").value;
+  const fullstackInterests = document.getElementById("fullstackInterests").value;
+  const multitasker = document.getElementById("multitasker").value;
+  const fullstackEvents = document.querySelector("input[name='fullstackEvents']:checked").value;
+  const fullstackInput = document.querySelector("input#fullstackInput").value;
+
+  if (fullstackExperience && fullstackInterests && multitasker && fullstackEvents && fullstackInput) {
+    document.querySelector("div#error").setAttribute("class", "hidden");
+    document.querySelector("div#result").removeAttribute("class");
+    let languageRecommendationResult = recommendationBackEndLanguage(fullstackExperience, fullstackInterests, multitasker, fullstackEvents);
+    document.querySelector("span#language").innerText = languageRecommendationResult;
+    //document.querySelector("li#experience").innerText = outputForFirstAnswerResult(currentExp);
+    //document.querySelector("span#aspect").innerText = frontEndInterests;
+    //document.querySelector("span#spa").innerText = spaSelection;
+    //document.querySelector("span#envision").innerText = projectEnvision;
+    //document.querySelector("span#preferring").innerText = languagePreference;
+
+  } else {
+    document.querySelector("div#error").removeAttribute("class");
+    document.querySelector("div#result").setAttribute("class", "hidden");
+    document.querySelector("div#error h3").textContent = "Please answer all questions";
+  }
+  document.getElementById("fullstackReset").removeAttribute("class");
+}
+
+function updateFullstackSubmitButtonStatus() {
+  const currentExp = document.querySelector("input[name='fullstackExperience']:checked").value;
+  const fullstackInterests = document.getElementById("fullstackInterests").value;
+  const multitasker = document.getElementById("multitasker").value;
+  const fullstackEvents = document.querySelector("input[name='fullstackEvents']:checked").value;
+  const backEndInput = document.querySelector("input#fullstackInput").value;
+  const submitButton = document.getElementById("fullstackSubmit");
+
+  if (currentExp && fullstackInterests && multitasker && fullstackEvents && backEndInput) {
+    submitButton.removeAttribute("disabled");
+  } else {
+    submitButton.setAttribute("disabled", "true");
+  }
+}
+
+function resetFullStackForm() {
+  document.getElementById("fullstackSubmit").setAttribute("disabled", "true");
+  document.querySelector("input[name='fullstackExperience'][value='fullstackBeginner']").checked = true;
+  document.getElementById("fullstackInterests").value = "endToEnd";
+  document.getElementById("multitasker").value = "codeJuggler"
+  document.querySelector("input[name='fullstackEvents'][value='codeRelay']").checked = true;
+  document.querySelector("input#fullstackInput").value = "";
+  document.querySelector("div#result").setAttribute("class", "hidden");
+  document.querySelector("div#error").setAttribute("class", "hidden");
+}
+
+
 window.addEventListener("load", function () {
   const developmentFocusForm = document.getElementById("developmentFocus");
   developmentFocusForm.addEventListener("click", handleUserChoice);
@@ -227,8 +331,10 @@ window.addEventListener("load", function () {
   developmentFocusForm.addEventListener("click", function () {
     resetFrontForm();
     resetBackForm();
+    resetFullStackForm();
     document.getElementById("frontReset").setAttribute("class", "hidden");
     document.getElementById("backReset").setAttribute("class", "hidden");
+    document.getElementById("fullstackReset").setAttribute("class", "hidden");
   });
 
   const frontEndInput = document.getElementById("frontEndInput");
@@ -248,5 +354,14 @@ window.addEventListener("load", function () {
 
   const backReset = document.getElementById("backReset");
   backReset.addEventListener("click", resetBackForm);
+
+  const fullstackInput = document.getElementById("fullstackInput");
+  fullstackInput.addEventListener("input", updateFullstackSubmitButtonStatus);
+
+  const fullstackFocusForm = document.getElementById("fullstackFocus");
+  fullstackFocusForm.addEventListener("submit", handleFullstackAnswers);
+
+  const fullstackReset = document.getElementById("fullstackReset");
+  fullstackReset.addEventListener("click", resetFullStackForm);
 });
 
